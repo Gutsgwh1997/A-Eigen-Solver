2d920e0407468c19158463640c19fc042bb1c3f2
Gutsgwh1997 <guanwenhao55@163.com>
2019-12-03 23:08:53 +0800

    增加了彩色输出，用const替换了define

--- utils/tic_toc.h
+++ utils/tic_toc.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <ctime>
-#include <cstdlib>
 #include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
 class TicToc {
 public:
@@ -23,3 +24,22 @@ public:
 private:
     std::chrono::time_point<std::chrono::system_clock> start, end;
 };
+
+//the following are UBUNTU/LINUX ONLY terminal color
+const std::string RESET("\033[0m");
+const std::string BLACK("\033[30m"); /* Black */
+const std::string RED("\033[31m"); /* Red */
+const std::string GREEN("\033[32m"); /* Green */
+const std::string YELLOW("\033[33m"); /* Yellow */
+const std::string BLUE("\033[34m"); /* Blue */
+const std::string MAGENTA("\033[35m"); /* Magenta */
+const std::string CYAN("\033[36m"); /* Cyan */
+const std::string WHITE("\033[37m"); /* White */
+const std::string BOLDBLACK("\033[1m\033[30m"); /* Bold Black */
+const std::string BOLDRED("\033[1m\033[31m"); /* Bold Red */
+const std::string BOLDGREEN("\033[1m\033[32m"); /* Bold Green */
+const std::string BOLDYELLOW("\033[1m\033[33m"); /* Bold Yellow */
+const std::string BOLDBLUE("\033[1m\033[34m"); /* Bold Blue */
+const std::string BOLDMAGENTA("\033[1m\033[35m"); /* Bold Magenta */
+const std::string BOLDCYAN("\033[1m\033[36m"); /* Bold Cyan */
+const std::string BOLDWHITE("\033[1m\033[37m"); /* Bold White */
